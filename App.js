6e7caa35de0cee3b1f050d19cfea5e2b61f174d7
munirudreamer <munirudreamer@gmail.com>
2024-10-01 13:42:58 +0000

    Initialized a new Expo-Project and setup Tailwind CSS

--- App.js
+++ App.js
@@ -3,8 +3,8 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View>
+      <Text className="items-center mt-5 justify-center bg-blue-500">Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
